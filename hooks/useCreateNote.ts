007326860f435d5e53f155a8ef4501b164007be3
d-levchenko <dl.qubb@gmail.com>
2026-06-02 20:19:10 +0300

    misspelled word fix

--- hooks/useCreateNote.ts
+++ hooks/useCreateNote.ts
@@ -1,5 +1,5 @@
 import noteService from '@/lib/api';
-import useNoteDraftStore from '@/lib/store/notsStore';
+import useNoteDraftStore from '@/lib/store/noteStore';
 
 import { QueryClient, useMutation } from '@tanstack/react-query';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
